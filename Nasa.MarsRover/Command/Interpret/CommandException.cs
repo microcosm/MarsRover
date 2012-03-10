04c77d7a6f61d3bcae2c2c8b2ab47c39f454b9a3
Andrew McWilliams <andy@jahya.net>
2012-03-10 00:58:31 +0000

    Removed unused CommandException constructor

--- Nasa.MarsRover/Command/Interpret/CommandException.cs
+++ Nasa.MarsRover/Command/Interpret/CommandException.cs
@@ -5,7 +5,6 @@ namespace Nasa.MarsRover.Command.Interpret
     [Serializable]
     public class CommandException : Exception
     {
-        public CommandException(string message) : base(message) {}
         public CommandException(string message, Exception innerException) : base(message, innerException) {}
     }
 }
